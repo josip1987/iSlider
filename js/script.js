1a88d764823f6bcf44e10e41316018f3221fe2b5
Josip Susic <josip.susic@hotmail.com>
2017-06-26 13:58:49 +0200

    animate

--- js/script.js
+++ js/script.js
@@ -21,10 +21,36 @@ $(document).ready(function() {
 		$('li.product').removeClass('active').addClass('inactive');
 		
 		// add active class to parent
+
 		$(this).parent().addClass('active');
 
 		var pos = $(this).parent().prevAll('.product').length;
 
-		$('#slides').stop().animate(marginLeft: -positions[pos] + 'px', 450);
+		$('#slides').stop().animate({marginLeft: -positions[pos] + 'px'}, 450);
+
+		// prevent default
+		e.preventDefault();
+
+		// stop autoscroll
+		if(!autoScroll) { clearInterval(itvl); }
 	});
+
+	// set first image as active
+	$('#menu ul li.product:first').addClass('active').siblings().addClass('inactive');
+
+	// autoscroll
+	var current = 1;
+	function autoScroll() {
+		if(current === -1) return false;
+
+		$('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click', [true]);
+
+		current++;
+	};
+
+	// duration for autoscroll
+	var duration = 5000;
+	var itvl = setInterval(function() {
+		autoScroll();
+	}, duration);
 });
